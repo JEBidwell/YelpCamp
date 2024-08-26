9ddb5cd49b40159021b58bc748de1036e6807458
Julian Falconi <julianfalconi@MacBook-Pro.local>
2024-08-26 13:41:30 -0400

    mongodb

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 
 //Link MongoAtlas for deployment
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl)
     .then(() => {
