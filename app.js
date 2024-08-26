ae43950d06e143db646c115794e6c6246f8edbac
Julian Falconi <julianfalconi@MacBook-Pro.local>
2024-08-26 12:16:08 -0400

    setting db

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 
 //Link MongoAtlas for deployment
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
